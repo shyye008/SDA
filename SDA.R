e8580a95fdcb67665b9474cc3a1604e579eeb636
Shangyuan Ye <114774102+shyye008@users.noreply.github.com>
2026-02-16 11:25:04 -0500

    Add files via upload

--- SDA.R
+++ SDA.R
@@ -2,53 +2,13 @@ library(glmnet);library(bootnet);library(arules);library(foreach)
 library(doParallel)
 #####R function#####
 ###Single hypothesis testing###
-SDA<-function(H,y,X,index,sig.level=0.05,L=1000){
-  ts.func<-function(x){
-    max(abs(x/sqrt(est.se)))
-  }
-  ts.func2<-function(x){
-    mean(abs(x/sqrt(est.se)))
-  }
-  
-  n<-length(y)
-  out<-c()
-  for (j in index) {
-    X_j<-X[,j];X_c<-X[,-j]
-    fit<-cv.glmnet(X_c,X_j)
-    pred<-c(predict(fit,X_c,s="lambda.min"))
-    KS<-c();CV<-c();CvM<-c();CV2<-c()
-    for (i in H) {
-      slice<-discretize(y,breaks=i,labels=F)
-      H.matrix<-matrix(0,nrow=length(slice),ncol=i)
-      H.matrix[cbind(seq_along(slice),slice)]<-1
-      m.Z<-kronecker(t(rep(1,i)),(X_j-pred))
-      nu<-colMeans(H.matrix*m.Z)
-      
-      m.nu<-kronecker(rep(1,n),t(nu))
-      inf.fun<-H.matrix*m.Z-m.nu
-      est.se<-diag(t(inf.fun)%*%inf.fun/n)
-      KS<-c(KS,max(abs(sqrt(n)*nu/sqrt(est.se))))
-      CvM<-c(CvM,mean(abs(sqrt(n)*nu/sqrt(est.se))))
-      
-      phi<-c()
-      for (l in 1:L) {
-        U<-matrix(rnorm(n*i),nrow=n)
-        phi_l<-colSums(U*inf.fun)/sqrt(n)
-        phi<-cbind(phi,phi_l)
-      }
-      
-      sim_max<-apply(phi,2,ts.func)
-      sim_mean<-apply(phi,2,ts.func2)
-      CV<-c(CV,quantile(sim_max,probs=1-sig.level))
-      CV2<-c(CV2,quantile(sim_mean,probs=1-sig.level))
-    }
-    
-    out<-cbind(out,rbind(KS,CV,CvM,CV2))
-  }
-  out
-}
-
-SDA_chi<-function(H,y,X,index,sig.level=0.05,L=1000){
+SDA<-function(H,y,X,index,method="Chi",L=1000){
+  ##H: number of slices
+  ##y: outcome variable
+  ##X: covariate matrix
+  ##index: index of variable(s) being tested
+  ##method: test statistic, can be either "Chi", "CvM", or "KS"
+  ##L: number of bootstrap replicates, only for method="CvM" or "KS"
   n<-length(y)
   out<-c()
   for (j in index) {
@@ -65,24 +25,64 @@ SDA_chi<-function(H,y,X,index,sig.level=0.05,L=1000){
       
       m.nu<-kronecker(rep(1,n),t(nu))
       inf.fun<-H.matrix*m.Z-m.nu
-      est.se<-t(inf.fun)%*%inf.fun/n
       
-      W<-n*t(nu)%*%solve(est.se)%*%nu
-      
-      pval<-c(pval,pchisq(W,i,lower.tail=F))
+      if(method=="Chi"){
+        est.se<-t(inf.fun)%*%inf.fun/n
+        W<-n*t(nu)%*%solve(est.se)%*%nu
+        pval<-c(pval,pchisq(W,i,lower.tail=F))
+      }else if(method=="CvM"){
+        est.se<-diag(t(inf.fun)%*%inf.fun/n)
+        ts.func<-function(x){
+          mean(abs(x/sqrt(est.se)))
+        }
+        CvM<-mean(abs(sqrt(n)*nu/sqrt(est.se)))
+        
+        phi<-c()
+        for (l in 1:L) {
+          U<-matrix(rnorm(n*i),nrow=n)
+          phi_l<-colSums(U*inf.fun)/sqrt(n)
+          phi<-cbind(phi,phi_l)
+        }
+        
+        sim_mean<-apply(phi,2,ts.func)
+        pval<-c(pval,sum(sim_mean>CvM)/L)
+      } else if(method=="KS"){
+        est.se<-diag(t(inf.fun)%*%inf.fun/n)
+        ts.func<-function(x){
+          max(abs(x/sqrt(est.se)))
+        }
+        KS<-max(abs(sqrt(n)*nu/sqrt(est.se)))
+        
+        phi<-c()
+        for (l in 1:L) {
+          U<-matrix(rnorm(n*i),nrow=n)
+          phi_l<-colSums(U*inf.fun)/sqrt(n)
+          phi<-cbind(phi,phi_l)
+        }
+        
+        sim_max<-apply(phi,2,ts.func)
+        pval<-c(pval,sum(sim_max>KS)/L)
+      }
     }
-    
+    ##out: a vector of p-values
     out<-c(out,pval)
   }
   out
 }
 
-###Mutiple hypothesis testing###
-SDA_knock<-function(y,X,H,index=c(1:p)){
+###Multiple hypothesis testing###
+SDA_knock<-function(y,X,H,method="Chi",feature="Diff",index=c(1:p),q=0.1){
+  ##H: number of slices
+  ##y: outcome variable
+  ##X: covariate matrix
+  ##method: test statistic, can be either "Chi", "CvM", or "KS"
+  ##feature: feature statistic, can be either "Diff" or "SM"
+  ##index: index of variable(s) being tested
+  ##q: FDR
   n<-length(y)
   slice<-discretize(y,breaks=H,labels=F)
   H.matrix<-model.matrix(y~as.factor(slice)-1)
-  SDA_CvM1<-c();SDA_CvM2<-c()
+  Stat=c()
   for (i in index) {
     X_i<-X[,i]
     X_c<-X[,-i]
@@ -98,34 +98,41 @@ SDA_knock<-function(y,X,H,index=c(1:p)){
     
     m.nu<-kronecker(rep(1,n),t(nu))
     inf.fun<-H.matrix*m.Z-m.nu
-    est.se<-diag(t(inf.fun)%*%inf.fun/n)
-    CvM<-mean(abs(sqrt(n)*nu/sqrt(est.se)))
     
     m.Z.Knock<-kronecker(t(rep(1,H)),Z_Knock)
     nu.Knock<-colMeans(H.matrix*m.Z.Knock)
-    
     m.nu.Knock<-kronecker(rep(1,n),t(nu.Knock))
     inf.fun.Knock<-H.matrix*m.Z.Knock-m.nu.Knock
-    est.se.Knock<-diag(t(inf.fun.Knock)%*%inf.fun.Knock/n)
-    CvM.Knock<-mean(abs(sqrt(n)*nu.Knock/sqrt(est.se.Knock)))
-    SDA_CvM1<-c(SDA_CvM1,CvM-CvM.Knock)
-    SDA_CvM2<-c(SDA_CvM2,sign(CvM-CvM.Knock)*max(CvM,CvM.Knock))
+    
+    if(method=="CvM"){
+      est.se<-diag(t(inf.fun)%*%inf.fun/n)
+      est.se.Knock<-diag(t(inf.fun.Knock)%*%inf.fun.Knock/n)
+      
+      TS<-mean(abs(sqrt(n)*nu/sqrt(est.se)))
+      TS.Knock<-mean(abs(sqrt(n)*nu.Knock/sqrt(est.se.Knock)))
+    }else if(method=="KS"){
+      est.se<-diag(t(inf.fun)%*%inf.fun/n)
+      est.se.Knock<-diag(t(inf.fun.Knock)%*%inf.fun.Knock/n)
+      
+      TS<-max(abs(sqrt(n)*nu/sqrt(est.se)))
+      TS.Knock<-max(abs(sqrt(n)*nu.Knock/sqrt(est.se.Knock)))
+    }else if(method=="Chi"){
+      est.se<-t(inf.fun)%*%inf.fun/n
+      est.se.Knock<-t(inf.fun.Knock)%*%inf.fun.Knock/n
+      
+      TS<-n*t(nu)%*%solve(est.se)%*%nu
+      TS.Knock<-n*t(nu.Knock)%*%solve(est.se.Knock)%*%nu.Knock
+    }
+    
+    if(feature=="Diff"){
+      Stat<-c(Stat,TS-TS.Knock)
+    }else if(feature=="SM"){
+      Stat<-c(Stat,sign(TS-TS.Knock)*max(TS,TS.Knock))
+    }
   }
-  list(SDA_CvM1,SDA_CvM2)
-}
-
-###Empirical power and FPR###
-Output<-function(Stat,beta,q=0.1){
+  
   FDP_hat<-function(Stat,t){
     sum(ifelse(Stat<=-t,1,0))/sum(ifelse(Stat>t,1,0))
-  }
-  
-  FDP<-function(beta,select){
-    sum(beta[select]==0)/length(select)
-  }
-  
-  power<-function(beta,select,size){
-    sum(beta[select]!=0)/size
   }
   
   p<-length(Stat)
@@ -136,9 +143,46 @@ Output<-function(Stat,beta,q=0.1){
   }
   tau<-candidate[min(which((FDP_all<=q)==T))]
   select<-(1:p)[Stat>tau]
-  fdp<-FDP(beta,select)
-  power<-power(beta,select,sum(beta))
-  data.frame(fdp,power)
+  
+  ##Stat: a vector of feature statistics
+  ##select: a vector of indexs for the variables being selected
+  list(Stat,select)
 }
 
+###Demo: SDA###
+library(LaplacesDemon)
+s1<-c(1,6,11,12,16,17)
+b1<-c(-0.4,0.6,-0.8,-0.8,1,1)
+n<-400;p<-1000
+Omega<-matrix(rep(0.5,5*5),nrow=5)
+diag(Omega)<-1
+Omega<-kronecker(diag(p/5),Omega)
+H<-5
 
+X<-rmvnp(n,rep(0,p),Omega)
+error<-rnorm(n)
+y<-sin(X[,s1]%*%b1)*exp(X[,s1]%*%b1)+error
+
+SDA(H,y,X,1:5,method="Chi")
+SDA(H,y,X,1:5,method="CvM")
+SDA(H,y,X,1:5,method="KS")
+
+###Demo: SDA_knock###
+n<-400;p<-1000
+H<-5
+s1<-1:6;b1<-rep(1,6)
+generator<-ggmGenerator()
+
+trueNet<-genGGM(p,nei=5,propPositive=0.8,p=0.25)
+X<-generator(n,trueNet)
+error<-rnorm(n)
+y<-sin(X[,s1]%*%b1)*exp(X[,s1]%*%b1)+0.5*error
+
+MH<-SDA_knock(y,X,H,method="Chi",feature="Diff",index=c(1:100))
+MH[[2]]
+MH2<-SDA_knock(y,X,H,method="Chi",feature="SM",index=c(1:100))
+MH2[[2]]
+MH3<-SDA_knock(y,X,H,method="CvM",feature="SM",index=c(1:100),q=0.15)
+MH3[[2]]
+MH4<-SDA_knock(y,X,H,method="KS",feature="Diff",index=c(1:100))
+MH4[[2]]
